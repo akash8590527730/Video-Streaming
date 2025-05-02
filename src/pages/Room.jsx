aa52950058800010494a2f56d09b9c13d5ac8355
akash8590527730 <akashkurian2020@gmail.com>
2025-05-02 10:50:58 +0530

    commit

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -3,47 +3,48 @@ import { useParams } from 'react-router-dom'
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
 const Room = () => {
-    let {roomId} = useParams()
-    function randomID(len) {
-        let result = '';
-        if (result) return result;
-        var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
-          maxPos = chars.length,
-          i;
-        len = len || 5;
-        for (i = 0; i < len; i++) {
-          result += chars.charAt(Math.floor(Math.random() * maxPos));
+  let { roomId } = useParams();
+
+  function randomID(len) {
+    let result = '';
+    if (result) return result;
+    var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+      maxPos = chars.length,
+      i;
+    len = len || 5;
+    for (i = 0; i < len; i++) {
+      result += chars.charAt(Math.floor(Math.random() * maxPos));
+    }
+    return result;
+  }
+
+  const appID = 1612030038;
+  const serverSecret = "19c884519c4d3a6cc8ce457dc3ad55b9";
+  const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId, randomID(5), randomID(5));
+
+  let myMeeting = async (element) => {
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    zp.joinRoom({
+      container: element,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.LiveStreaming,
+      },
+      sharedLinks: [
+        {
+          name: "copy link",
+          url: `https://video-streaming-ecru.vercel.app/room/${roomId}`
         }
-        return result;
-      }
-    const appID =1612030038
-    const serverSecret = "19c884519c4d3a6cc8ce457dc3ad55b9"
-    const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId,  randomID(5),  randomID(5));
+      ]
+    });
+  };
 
-
-    let myMeeting = async (element) => {
-        // Create instance object from Kit Token.
-        const zp = ZegoUIKitPrebuilt.create(kitToken);
-        // start the call
-        zp.joinRoom({
-          container: element,
-          scenario: {
-            mode: ZegoUIKitPrebuilt.LiveStreaming,
-        
-          },
-          sharedLinks:[
-            {name:"copy link",
-            url:`https://video-streaming-ecru.vercel.app/room/${roomId}`}
-          ]
-        });
-    };
   return (
     <div
-    className="myCallContainer"
-    ref={myMeeting}
-    style={{ width: '100vw', height: '100vh' }}
-  ></div>
-  )
-}
+      className="myCallContainer"
+      ref={myMeeting}
+      style={{ width: '100vw', height: '100vh' }}
+    ></div>
+  );
+};
 
-export default Room
+export default Room;
